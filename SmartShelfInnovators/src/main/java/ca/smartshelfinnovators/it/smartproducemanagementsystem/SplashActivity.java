4a1c5db55fd8e4d0cfc813bb75c94d75f1d00e13
Riyan1102 <riyane0152@gmail.com>
2024-09-27 23:14:33 -0400

    Added an SplashActivity that show an splash screen for 3 second before going to main activity.

---
+++ SmartShelfInnovators/src/main/java/ca/smartshelfinnovators/it/smartproducemanagementsystem/SplashActivity.java
@@ -0,0 +1,30 @@
+package ca.smartshelfinnovators.it.smartproducemanagementsystem;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Delay for 3 seconds, then transition to MainActivity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // Finish splash activity so it doesn't stay in the backstack
+            }
+        }, 3000); // 3000 milliseconds = 3 seconds
+    }
+}
