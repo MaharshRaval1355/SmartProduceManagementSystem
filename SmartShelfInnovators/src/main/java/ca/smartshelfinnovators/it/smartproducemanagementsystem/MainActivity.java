a8468e2139f60ddc65adf4ac424e38f2a41f9017
winsotuke <n01544313@humber.ca>
2024-11-12 17:54:13 -0500

    Tryed to make the top menu bar but it does not show on the output on top just bottom menu ~ Winso

--- SmartShelfInnovators/src/main/java/ca/smartshelfinnovators/it/smartproducemanagementsystem/MainActivity.java
+++ SmartShelfInnovators/src/main/java/ca/smartshelfinnovators/it/smartproducemanagementsystem/MainActivity.java
@@ -1,13 +1,20 @@
 package ca.smartshelfinnovators.it.smartproducemanagementsystem;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 import ca.smartshelfinnovators.it.smartproducemanagementsystem.DashboardFragment;
 import ca.smartshelfinnovators.it.smartproducemanagementsystem.InventoryFragment;
@@ -17,44 +24,94 @@ import ca.smartshelfinnovators.it.smartproducemanagementsystem.SettingsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
-        bottomNav.setOnItemSelectedListener(navListener);
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int itemId = item.getItemId();
+            if (itemId == R.id.menu_dashboard) {
+                selectedFragment = new DashboardFragment();
+            } else if (itemId == R.id.menu_regulators) {
+                selectedFragment = new RegulatorsFragment();
+            } else if (itemId == R.id.menu_inventory) {
+                selectedFragment = new InventoryFragment();
+            } else if (itemId == R.id.menu_settings) {
+                selectedFragment = new SettingsFragment();
+            }
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            }
+            return true;
+        });
 
         // Set the default fragment to Dashboard
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 new DashboardFragment()).commit();
     }
 
-    // Bottom Navigation Listener using the updated if-else logic
-    private final BottomNavigationView.OnItemSelectedListener navListener =
-            new BottomNavigationView.OnItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    Fragment selectedFragment = null;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
 
-                    // Use if-else instead of switch-case to handle item selection
-                    int itemId = item.getItemId();
-                    if (itemId == R.id.menu_dashboard) {
-                        selectedFragment = new DashboardFragment();
-                    } else if (itemId == R.id.menu_regulators) {
-                        selectedFragment = new RegulatorsFragment();
-                    } else if (itemId == R.id.menu_inventory) {
-                        selectedFragment = new InventoryFragment();
-                    } else if (itemId == R.id.menu_settings) {
-                        selectedFragment = new SettingsFragment();
-                    }
+        if (itemId == R.id.action_home) {
+            Toast.makeText(this, "Home selected", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (itemId == R.id.action_settings) {
+            openSettings();
+            return true;
+        } else if (itemId == R.id.action_help) {
+            showHelpDialog();
+            return true;
+        } else if (itemId == R.id.action_profile) {
+            requestLocationPermission();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
 
-                    if (selectedFragment != null) {
-                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                                selectedFragment).commit();
-                    }
+    private void requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        } else {
+            showSnackbar("Location permission already granted");
+        }
+    }
 
-                    return true;
-                }
-            };
+    private void openSettings() {
+        Toast.makeText(this, "Settings opened", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                showSnackbar("Location permission granted");
+            } else {
+                showSnackbar("Location permission denied");
+            }
+        }
+    }
+
+    private void showSnackbar(String message) {
+        Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG).show();
+    }
+
+    private void showHelpDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Help")
+                .setMessage("This is the help section.")
+                .setPositiveButton("OK", (dialog, which) -> dialog.dismiss())
+                .show();
+    }
 }
