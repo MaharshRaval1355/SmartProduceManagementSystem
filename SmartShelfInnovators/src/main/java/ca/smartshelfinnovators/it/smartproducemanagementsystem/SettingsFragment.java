bb828e3ddd8824e10ed8ae05759854e84b6a4241
Riyan1102 <riyane0152@gmail.com>
2024-11-12 00:41:53 -0500

    Updated SettingsFragment.java and fragment_settings.xml. By adding two functionalities and 3 Textview for show. Functionalities include Dark Mode and Lock screen. Lastly, updated strings.xml for both english and french language.

--- SmartShelfInnovators/src/main/java/ca/smartshelfinnovators/it/smartproducemanagementsystem/SettingsFragment.java
+++ SmartShelfInnovators/src/main/java/ca/smartshelfinnovators/it/smartproducemanagementsystem/SettingsFragment.java
@@ -1,16 +1,77 @@
 package ca.smartshelfinnovators.it.smartproducemanagementsystem;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
+
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
 public class SettingsFragment extends Fragment {
 
+    View view;
+    protected SharedPreferences.Editor editor;
+    protected Switch lockScreenSwitch;
+    protected Switch darkModeSwitch;
+    protected boolean isDarkModeEnabled;
+    protected boolean isLockScreenPortrait;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        darkModeSwitch = view.findViewById(R.id.darkMode_switch);
+        lockScreenSwitch = view.findViewById(R.id.lockScreen_switch);
+
+        getContext();
+        SharedPreferences sharedPreferences = requireContext().getSharedPreferences("SettingsPrefs", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+        isDarkModeEnabled = sharedPreferences.getBoolean("dark_mode", false);
+        isLockScreenPortrait = sharedPreferences.getBoolean("lock_screen", false);
+
+        darkModeSwitch.setChecked(isDarkModeEnabled);
+        lockScreenSwitch.setChecked(isLockScreenPortrait);
+
+        applyDarkMode(isDarkModeEnabled);
+        applyLockScreenMode(isLockScreenPortrait);
+
+        darkModeSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            editor.putBoolean("dark_mode", isChecked);
+            editor.apply();
+            applyDarkMode(isChecked);
+        });
+
+        lockScreenSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            editor.putBoolean("lock_screen", isChecked);
+            editor.apply();
+            applyLockScreenMode(isChecked);
+        });
+
+        return view;
+    }
+
+    private void applyDarkMode(boolean isEnabled) {
+        if (isEnabled) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+    }
+
+    // Method to apply Lock Screen orientation
+    private void applyLockScreenMode(boolean isPortrait) {
+        if (isPortrait) {
+            requireActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        } else {
+            requireActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+        }
     }
 }
