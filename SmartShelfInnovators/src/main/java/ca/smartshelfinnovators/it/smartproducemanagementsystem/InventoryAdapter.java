518e48f9424b6ee8065f8fd81f8c2d75393d9bc5
MaharshRaval1355 <n01391355@gmail.com>
2024-12-06 01:23:08 -0500

    InventoryAdapter.java class is an adapter class created to bind the inventory data to the recycler view.

---
+++ SmartShelfInnovators/src/main/java/ca/smartshelfinnovators/it/smartproducemanagementsystem/InventoryAdapter.java
@@ -0,0 +1,66 @@
+package ca.smartshelfinnovators.it.smartproducemanagementsystem;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class InventoryAdapter extends RecyclerView.Adapter<InventoryAdapter.InventoryViewHolder> {
+
+    private Context context;
+    private List<InventoryItem> inventoryList;
+
+    public InventoryAdapter(Context context, List<InventoryItem> inventoryList) {
+        this.context = context;
+        this.inventoryList = inventoryList;
+    }
+
+    @NonNull
+    @Override
+    public InventoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_inventory, parent, false);
+        return new InventoryViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull InventoryViewHolder holder, int position) {
+        InventoryItem item = inventoryList.get(position);
+        holder.itemName.setText(item.getItemName());
+        holder.stockLevel.setText(item.getStockLevel());
+        holder.status.setText(item.getStatus());
+
+        // Set color based on status
+        switch (item.getStatus().toLowerCase()) {
+            case "low":
+                holder.status.setTextColor(context.getResources().getColor(android.R.color.holo_red_dark));
+                break;
+            case "optimal":
+                holder.status.setTextColor(context.getResources().getColor(android.R.color.holo_green_dark));
+                break;
+            case "high":
+                holder.status.setTextColor(context.getResources().getColor(android.R.color.holo_blue_dark));
+                break;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return inventoryList.size();
+    }
+
+    public static class InventoryViewHolder extends RecyclerView.ViewHolder {
+        TextView itemName, stockLevel, status;
+
+        public InventoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+            itemName = itemView.findViewById(R.id.item_name);
+            stockLevel = itemView.findViewById(R.id.item_stock);
+            status = itemView.findViewById(R.id.item_status);
+        }
+    }
+}
+
