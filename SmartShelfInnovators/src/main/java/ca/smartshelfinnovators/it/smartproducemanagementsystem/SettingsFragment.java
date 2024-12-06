f6a53174c57745f5e267244b63aafb5e334c864e
Riyan1102 <riyane0152@gmail.com>
2024-12-06 01:48:10 -0500

    Updated the code to retrieve the username from firebase firestone database.

--- SmartShelfInnovators/src/main/java/ca/smartshelfinnovators/it/smartproducemanagementsystem/SettingsFragment.java
+++ SmartShelfInnovators/src/main/java/ca/smartshelfinnovators/it/smartproducemanagementsystem/SettingsFragment.java
@@ -25,11 +25,6 @@ import androidx.fragment.app.Fragment;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -56,7 +51,7 @@ public class SettingsFragment extends Fragment {
         // Initialize user information
         userNameTV = view.findViewById(R.id.user_name);
         // Get the current user
-        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
 
         if (currentUser != null) {
             String uid = currentUser.getUid(); // Get user UID
@@ -74,7 +69,6 @@ public class SettingsFragment extends Fragment {
                     if (document.exists()) {
                         // Retrieve fields
                         String name = document.getString("name");
-                        String email = document.getString("email");
 
                         // Display data in TextView
                         userNameTV.setText(name != null ? name : "Guest User");
